0ffb26401e8820bd26872c8df280068603090a4a
mandeepharsh <harshman47@gmail.com>
2023-06-14 20:00:53 -0400

    adds status column to material table

--- migrations/20230614012501_Material.js
+++ migrations/20230614012501_Material.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
       table.string('material_number').notNullable();
       table.integer('quantity').notNullable();
       table.string('size').notNullable();
-      table.boolean('status').defaultTo(false);
+      table.string('status').notNullable();
       table.date('receive_date').nullable();
       table.integer('work_order_id').unsigned().notNullable();
       table.foreign('work_order_id').references('work_order_id').inTable('WorkOrder').onDelete('CASCADE');
