776f4e7ecd8fb0aa8e7b4aaf7be6271a398dde95
mandeepharsh <harshman47@gmail.com>
2023-11-07 22:25:50 -0500

    create a one on one chat

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,46 +1,15 @@
 const knex = require("knex")(require("../knexfile"));
 require("dotenv").config();
-// I want to check if there is chat of both the users with each other
 
-// const accessChat = async (req, res) => {
-//   const username = req.username;
-//   const chatMemberId = req.body.member_username;
-//   // if the chat betweem the user and the user exsist then send that chat back other wise create a new chat
-
-//   if (!username && !chatMemberId) {
-//     return res.status(400).send("Username and chat member ID are required.");
-//   }
-//   const logedInUser = await knex("users")
-//     .join("employee", "employee.employee_id", " users.employee_id")
-//     .where({ user_username: username });
-//  if (logedInUser.length === 0){
-//   return res.status(404).send("User not found.");
-//  }
-//     // get the name of the user
-//   const chatMemeber = await knex("users").where({
-//     user_username: chatMemberId,
-//   });
-//   // if the chat memeber we are trying to acess dosent' exsist
-//   if (chatMemeber.length === 0){
-//     return res.status(404).send("User not found.");
-//    }
-
-//   const userID = [logedInUser[0].user_id, chatMemeber[0].user_id];
-//   const chats = await knex("ChatParticipants")
-//     .join("Chats", "Chats.ChatID", "ChatParticipants.ChatID")
-//     .whereIn("ChatParticipants.userID", userID);
-// };
 const accessChat = async (req, res) => {
   const username = req.username;
   const chatMemberId = req.body.member_username;
-  // if the chat between the user and the user exists, then send that chat back; otherwise, create a new chat
 
   if (!username || !chatMemberId) {
     return res.status(400).send("Username and chat member ID are required.");
   }
 
   try {
-    // Check if the logged-in user exists
     const loggedInUser = await knex("Users")
       .join("Employee", "Employee.employee_id", "Users.employee_id")
       .where({ user_username: username });
@@ -49,7 +18,6 @@ const accessChat = async (req, res) => {
       return res.status(404).send("Logged-in user not found.");
     }
 
-    // Check if the chat member exists
     const chatMember = await knex("Users").where({
       user_username: chatMemberId,
     });
@@ -60,10 +28,8 @@ const accessChat = async (req, res) => {
 
     const userID = [loggedInUser[0].user_id, chatMember[0].user_id];
 
-    // Check if a one-on-one chat already exists or create a new one
     const chats = await checkAndCreateOneOnOneChat(userID[0], userID[1]);
 
-    // Now you can send the 'chats' data back as a response or perform other actions.
     return res.status(200).json({ chatID: chats });
   } catch (error) {
     console.error("Error accessing chat:", error);
@@ -71,15 +37,25 @@ const accessChat = async (req, res) => {
   }
 };
 
-// Function to check and create one-on-one chat (place this outside the accessChat function)
 async function checkAndCreateOneOnOneChat(userID1, userID2) {
-  // Check if a one-on-one chat exists between the two users
-  const existingChat = await knex("Chats").where({ IsGroupChat: false });
+  function findChatIDForUsers(userID1, userID2, data) {
+    const chatIDs = {};
+    for (const entry of data) {
+      if (entry.UserID === userID1 || entry.UserID === userID2) {
+        if (chatIDs[entry.ChatID]) {
+          return entry.ChatID;
+        }
+        chatIDs[entry.ChatID] = true;
+      }
+    }
+    return null;
+  }
 
-  console.log(existingChat);
+  const chats = await knex("ChatParticipants");
+  const existingChat = findChatIDForUsers(userID1, userID2, chats);
 
   if (existingChat) {
-    return existingChat.ChatID;
+    return existingChat;
   } else {
     const newChat = await knex("Chats").insert({
       ChatName: "One-on-One Chat",
